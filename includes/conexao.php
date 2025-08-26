49298556be1e71648f1cd6fd4350bee8b87682c4
Amorim <168141364+Amoraa16@users.noreply.github.com>
2025-08-26 09:17:01 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
-$banco = "docaria"; // nome do seu banco
+$banco = "docaria";
 
 $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
