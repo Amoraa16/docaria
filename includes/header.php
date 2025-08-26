bcddf5b57b9fb0af92f6c5ad1288233038da4c2e
Amorim <168141364+Amoraa16@users.noreply.github.com>
2025-08-26 09:20:01 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,20 +9,21 @@ ini_set('display_errors', 0);
 <head>
   <meta charset="UTF-8">
   <title>Doçaria Amorim</title>
- <link rel="stylesheet" href="/doces/css/style.css">
+ <link rel="stylesheet" href="css/style.css">
 
 <header>
     <div class="header-content">
-        <a href="/doces/index.php" class="logo-title-link">
-            <img src="/doces/imagens/logo.png" alt="Logo Doçaria Amorim" class="logo">
+        <a href="index.php" class="logo-title-link">
+            <img src="imagens/logo.png" alt="Logo Doçaria Amorim" class="logo">
             <h1>Doçaria Amorim</h1>
         </a>
     </div>
     <nav>
-        <a href="/doces/index.php">Início</a>
-        <a href="/doces/pages/produtos.php">Produtos</a>
-        <a href="/doces/pages/carrinho.php">Carrinho</a>
+        <a href="index.php">Início</a>
+        <a href="pages/produtos.php">Produtos</a>
+        <a href="pages/carrinho.php">Carrinho</a>
     </nav>
 </header>
+
 
 
